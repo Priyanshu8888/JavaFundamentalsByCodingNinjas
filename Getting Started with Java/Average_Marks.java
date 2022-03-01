4ba124d09eead54b0130339328160fb3fbd6ffa1
Priyanshu8888 <priyanshu.karmakar.fiem.ece19@teamfuture.in>
2022-03-01 18:54:08 +0530

    Initial commit

---
+++ Getting Started with Java/Average_Marks.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+public class Average_Marks {
+
+
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+        
+        int m1, m2, m3;
+        
+        Scanner s = new Scanner(System.in);
+        
+        String str = s.next();
+        char ch = str.charAt(0);
+        m1 = s.nextInt();
+        m2 = s.nextInt();
+        m3 = s.nextInt();
+        System.out.println(ch);
+        int average = (m1 + m2 + m3)/3;
+        System.out.print(average);
+
+		
+	}
+
+}
